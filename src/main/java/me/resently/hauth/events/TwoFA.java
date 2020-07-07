6e915a80fc841e93ce6a10311c1dc95af11c600a
Resently <danijose1803@gmail.com>
2020-07-07 09:19:13 -0500

    Cleaned up code

--- src/main/java/me/resently/hauth/events/TwoFA.java
+++ src/main/java/me/resently/hauth/events/TwoFA.java
@@ -18,8 +18,8 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class TwoFA implements Listener {
-    private ArrayList<UUID> locked = new ArrayList<>();
-    private ArrayList<Player> joined = new ArrayList<>();
+    private final ArrayList<UUID> locked = new ArrayList<>();
+    private final ArrayList<Player> joined = new ArrayList<>();
     hAuth plugin;
 
     public TwoFA(hAuth plugin) {
@@ -27,8 +27,8 @@ public class TwoFA implements Listener {
     }
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e){
-        if (plugin.getConfig().getBoolean("2FA-users")){
+    public void onJoin(PlayerJoinEvent e) {
+        if (plugin.getConfig().getBoolean("2FA-users")) {
             Player player = e.getPlayer();
             if (!joined.contains(player)) {
                 if (!plugin.getconfigManager.getUserData().contains("authcodes." + player.getUniqueId())) {
@@ -40,11 +40,11 @@ public class TwoFA implements Listener {
 
                     plugin.getconfigManager.getUserData().set("authcodes." + player.getUniqueId(), googleAuthenticatorKey.getKey());
                     plugin.getconfigManager.saveuserdata();
-                } else {
-                    if (plugin.getconfigManager.getUserData().contains("authcodes." + player.getUniqueId())) {
-                        locked.add(player.getUniqueId());
-
-                    }
+                }
+            } else {
+                if (plugin.getconfigManager.getUserData().contains("authcodes." + player.getUniqueId())) {
+                    locked.add(player.getUniqueId());
+                    player.sendMessage(ChatColor.RED + "Open Google Authenticator app and provide the code!");
                 }
             }
         }
@@ -58,14 +58,16 @@ public class TwoFA implements Listener {
                         GoogleAuthenticatorKey googleAuthenticatorKey = googleAuthenticator.createCredentials();
 
                         player.sendMessage(ChatColor.GREEN + "Your Google authenticator code is : " + googleAuthenticatorKey.getKey());
-                        player.sendMessage(ChatColor.YELLOW + "Please enter this code in the Google Authenticator App before leaving the server");
+                        player.sendMessage(ChatColor.YELLOW + "Please enter this code in the Google Authenticator App before leaving the server!");
 
                         plugin.getconfigManager.getUserData().set("authcodes." + player.getUniqueId(), googleAuthenticatorKey.getKey());
                         plugin.getconfigManager.saveuserdata();
                     }
                 } else {
-                    locked.add(player.getUniqueId());
-                    player.sendMessage(ChatColor.RED + "Open Google Authenticator app and provide the code!");
+                    if (plugin.getconfigManager.getUserData().contains("authcodes." + player.getUniqueId())) {
+                        locked.add(player.getUniqueId());
+                        player.sendMessage(ChatColor.RED + "Open Google Authenticator app and provide the code!");
+                    }
                 }
             }
         }
@@ -92,7 +94,7 @@ public class TwoFA implements Listener {
 
         if (locked.contains(player.getUniqueId())){
             try {
-                Integer code = Integer.parseInt(message);
+                int code = Integer.parseInt(message);
                 if (playercode(player, code)){
                     locked.remove(player.getUniqueId());
                     player.sendMessage(ChatColor.GREEN +"Access granted! Welcome" + player.getDisplayName());
@@ -107,25 +109,25 @@ public class TwoFA implements Listener {
     }
 
     @EventHandler
-    public void playermoves(PlayerMoveEvent e){
+    public void playerMoves(PlayerMoveEvent e) {
         Player player = e.getPlayer();
-        if (locked.contains(player.getUniqueId())){
+        if (locked.contains(player.getUniqueId())) {
             player.teleport(e.getFrom());
         }
     }
 
     @EventHandler
-    public void playerbreaksblock(BlockBreakEvent e){
+    public void playerBreaksBlock(BlockBreakEvent e) {
         Player player = e.getPlayer();
-        if (locked.contains(player.getUniqueId())){
+        if (locked.contains(player.getUniqueId())) {
             e.setCancelled(true);
         }
     }
 
     @EventHandler
-    public void playerplaceblock(BlockPlaceEvent e){
+    public void playerPlaceBlock(BlockPlaceEvent e) {
         Player player = e.getPlayer();
-        if (locked.contains(player.getUniqueId())){
+        if (locked.contains(player.getUniqueId())) {
             e.setCancelled(true);
         }
     }
