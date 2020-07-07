ef9e970b11fe519c120fefb1e92e3a3fb56f8743
Resently <danijose1803@gmail.com>
2020-07-07 08:57:05 -0500

    Changed 2FA method of creation

--- src/main/java/me/resently/hauth/events/TwoFA.java
+++ src/main/java/me/resently/hauth/events/TwoFA.java
@@ -19,6 +19,7 @@ import java.util.UUID;
 
 public class TwoFA implements Listener {
     private ArrayList<UUID> locked = new ArrayList<>();
+    private ArrayList<Player> joined = new ArrayList<>();
     hAuth plugin;
 
     public TwoFA(hAuth plugin) {
@@ -29,7 +30,7 @@ public class TwoFA implements Listener {
     public void onJoin(PlayerJoinEvent e){
         if (plugin.getConfig().getBoolean("2FA-users")){
             Player player = e.getPlayer();
-            if (!player.hasPlayedBefore()) {
+            if (!joined.contains(player)) {
                 if (!plugin.getconfigManager.getUserData().contains("authcodes." + player.getUniqueId())) {
                     GoogleAuthenticator googleAuthenticator = new GoogleAuthenticator();
                     GoogleAuthenticatorKey googleAuthenticatorKey = googleAuthenticator.createCredentials();
@@ -40,25 +41,29 @@ public class TwoFA implements Listener {
                     plugin.getconfigManager.getUserData().set("authcodes." + player.getUniqueId(), googleAuthenticatorKey.getKey());
                     plugin.getconfigManager.saveuserdata();
                 } else {
-                    locked.add(player.getUniqueId());
-                    player.sendMessage(ChatColor.RED + "Open Google Authenticator app and provide the code!");
+                    if (plugin.getconfigManager.getUserData().contains("authcodes." + player.getUniqueId())) {
+                        locked.add(player.getUniqueId());
+
+                    }
                 }
             }
         }
 
         if (plugin.getConfig().getBoolean("2FA-staff")){
             Player player = e.getPlayer();
-            if (player.hasPermission("2FA.staff")){
-                if (!plugin.getconfigManager.getUserData().contains("authcodes." + player.getUniqueId())){
-                    GoogleAuthenticator googleAuthenticator = new GoogleAuthenticator();
-                    GoogleAuthenticatorKey googleAuthenticatorKey = googleAuthenticator.createCredentials();
+            if (player.hasPermission("2FA.staff")) {
+                if (!joined.contains(player)) {
+                    if (!plugin.getconfigManager.getUserData().contains("authcodes." + player.getUniqueId())) {
+                        GoogleAuthenticator googleAuthenticator = new GoogleAuthenticator();
+                        GoogleAuthenticatorKey googleAuthenticatorKey = googleAuthenticator.createCredentials();
 
-                    player.sendMessage(ChatColor.GREEN + "Your Google authenticator code is : " + googleAuthenticatorKey.getKey());
-                    player.sendMessage(ChatColor.YELLOW + "Please enter this code in the Google Authenticator App before leaving the server");
+                        player.sendMessage(ChatColor.GREEN + "Your Google authenticator code is : " + googleAuthenticatorKey.getKey());
+                        player.sendMessage(ChatColor.YELLOW + "Please enter this code in the Google Authenticator App before leaving the server");
 
-                    plugin.getconfigManager.getUserData().set("authcodes." + player.getUniqueId(), googleAuthenticatorKey.getKey());
-                    plugin.getconfigManager.saveuserdata();
-                }else {
+                        plugin.getconfigManager.getUserData().set("authcodes." + player.getUniqueId(), googleAuthenticatorKey.getKey());
+                        plugin.getconfigManager.saveuserdata();
+                    }
+                } else {
                     locked.add(player.getUniqueId());
                     player.sendMessage(ChatColor.RED + "Open Google Authenticator app and provide the code!");
                 }
@@ -105,7 +110,7 @@ public class TwoFA implements Listener {
     public void playermoves(PlayerMoveEvent e){
         Player player = e.getPlayer();
         if (locked.contains(player.getUniqueId())){
-            e.setCancelled(true);
+            player.teleport(e.getFrom());
         }
     }
 
